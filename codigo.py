f1e1eaac3ed505cb13444a8e0610fe6342f2c959
Marcos Felipe <132904349+felipeillaoi@users.noreply.github.com>
2023-06-09 19:32:34 -0300

    luiz 09/06

--- codigo.py
+++ codigo.py
@@ -1,11 +1,12 @@
 import tkinter as tk
 from tkinter import messagebox
 import sys
-import sqlite3
 from login_exists import login_existe
 from inserir import inserir_dado
 from mudar import mudar_dado
 from ver import ver_dado
+import sqlite3
+import tkinter.font as tkfont
 
 def verificar_login():
     
@@ -69,6 +70,9 @@ def login():
 def pressionar_enter(event):
     botao1.invoke()
 
+def pressionar_enter_delete(event):
+    botao_enter_del.invoke()
+
 def fechar():
     tela_login.destroy()
     sys.exit()
@@ -82,6 +86,9 @@ def criar_tela():
 
 def fechar_adicionar():
     tela_adicionar.destroy()
+
+def fechar_deletar():
+    tela_deletar.destroy()
    
 def criar_menu():
    
@@ -98,6 +105,7 @@ def criar_menu():
     opcoes_menu.add_command(label="Config")
     opcoes_menu.add_command(label="Editar")
     opcoes_menu.add_command(label="Adicionar", command=adicionar_tela)
+    opcoes_menu.add_command(label="Deletar", command=deletar_tela)
 
 def botao3teste():
     global criar_login
@@ -117,6 +125,20 @@ def botao3teste():
             inserir_dado(criar_login, criar_senha, criar_nome)
             messagebox.showinfo("Sucesso!","Novo login adicionado!")
             fechar_adicionar()
+
+def botao_del_teste():
+    
+    global deletar_login # Local para por as variáveis globais.
+    deletar_login = usuario_del.get() # deletar_login recebendo de usuário_gel.get()
+    
+    # Início da condição para deletar um usuário
+    if deletar_login.strip() == '':
+        messagebox.showerror("Erro", "Por favor, preencha todos os campos.") # Msgbox caso n tenha nada escrito.
+        deletar_login.focus()
+    elif login_existe(deletar_login):
+        excluir_dado() # Aqui acontece a magia.
+    else:
+        messagebox.showerror("Erro!", "Usuario não existente. Digite novamente.") # Msgbox caso n tenha esse usuario no banco.
 
 def adicionar_tela():
     
@@ -152,6 +174,53 @@ def adicionar_tela():
 
     botao3 = tk.Button(tela_adicionar, text="Cancelar", width=10, height=1, command=fechar_adicionar, bg="#ff3333", activebackground="#ff6666")
     botao3.place(relx=0.6, rely=0.8)
+
+def excluir_dado():
+
+    # Conexão com o banco de dados
+    conn = sqlite3.connect('Banco_PY.db')
+    cursor = conn.cursor()
+
+    # Capturar o dado a ser excluído
+    dado_para_excluir = usuario_del.get()
+
+    # Executar o comando de exclusão na tabela funcionario
+    cursor.execute("DELETE FROM Funcionario WHERE login=?", (dado_para_excluir,))
+    messagebox.showinfo("Sucesso", "Usuario deletado!")
+
+    # Salvar as alterações e fechar a conexão com o banco
+    conn.commit()
+    conn.close()
+    
+def deletar_tela():
+    
+    # Iniciando conexão com o banco.
+    conn = sqlite3.connect('Banco_PY.db')
+    cursor = conn.cursor()
+    
+    # Variáveis globais + criando tela para excluir o dado.
+    global tela_deletar, botao_enter_del, resultado_del, usuario_del, entrada_usuario_del
+    tela_deletar = tk.Toplevel(master=tela)
+    tela_deletar.title("Deletando Usuário")
+    tela_deletar.geometry("400x300+700+200")
+    tela_deletar.resizable(0,0)
+    tela_deletar.transient(tela)
+    
+    # Variável que é convertida para string onde tem abaixo "textvariable=usuario_del". N entendi mt XD
+    usuario_del = tk.StringVar()
+
+    # Inserindo a label e após, colocando o espaço para digitar o nome do usuário para ser deletado.
+    label_del_nome = tk.Label(tela_deletar, text="Digite o nome/login do usuário:")
+    label_del_nome.place(relx=0.25, rely=0.1, anchor=tk.CENTER)
+    entrada_usuario_del = tk.Entry(tela_deletar, textvariable=usuario_del)
+    entrada_usuario_del.place(relx=0.2, rely=0.2, anchor=tk.CENTER)
+    entrada_usuario_del.focus()
+
+    # Botão que ativa clicando no enter. Daqui verifique a função botao_del_teste.
+    botao_enter_del = tk.Button(tela_deletar, text="Enter", width=10, height=1, command=botao_del_teste)
+    botao_enter_del.place(relx=0.5, rely=0.2, anchor=tk.CENTER)
+    tela_deletar.bind('<Return>', pressionar_enter_delete)
+
 
 criar_tela()
 tela.withdraw()
